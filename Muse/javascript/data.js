5abb4395ac3ec41e171531272ede5f4d5fcc8012
aidanaikk <96576921+aidanaikk@users.noreply.github.com>
2022-02-20 21:22:19 +0600

    Add files via upload

---
+++ Muse/javascript/data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'song 1',
+        path: 'assets/audio/song1.mp3',
+        artist: 'artist 1',
+        cover: 'assets/images/img1.jpeg'
+    },
+    {
+        name: 'song 2',
+        path: 'assets/audio/song2.mp3',
+        artist: 'artist 2',
+        cover: 'assets/images/img2.jpeg'
+    },
+    {
+        name: 'song 3',
+        path: 'assets/audio/song3.mp3',
+        artist: 'artist 3',
+        cover: 'assets/images/img3.jpeg'
+    },
+    {
+        name: 'song 4',
+        path: 'assets/audio/song4.mp3',
+        artist: 'artist 4',
+        cover: 'assets/images/img4.jpeg'
+    },
+    {
+        name: 'song 5',
+        path: 'assets/audio/song5.mp3',
+        artist: 'artist 5',
+        cover: 'assets/images/img5.jpeg'
+    },
+]
---
+++ Muse/javascript/muse.js
@@ -0,0 +1,8 @@
+<script>
+jQuery(function($){
+$('.musicstart').on('click', function(){
+$('.music')[0].play()
+});
+$('.music').hide();
+});
+</script>
